ffaa6ccf5b551f5b3cdc48a4ec57ac8a99ec1e7f
Hassaan-ahmad-ch <hassaan.ahmad.ch.2016@gmail.com>
2024-06-29 00:58:47 +0000

    added gunicorn log/config files

---
+++ gunicorn_config.py
@@ -0,0 +1,8 @@
+
+bind = 'unix:/home/ubuntu/hblog/blog.sock'  
+workers = 3
+timeout = 30
+loglevel = 'info'
+errorlog = '/home/ubuntu/hblog/gunicorn_error.log'
+accesslog = '/home/ubuntu/hblog/gunicorn_access.log'
+
